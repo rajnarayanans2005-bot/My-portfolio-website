17f875e94be04954e7f14585e86da98e1b46da4b
Raj Narayanan <rajnarayanan@gmail.com>
2026-04-17 19:24:02 +0530

    Added root route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
+app.get("/", (req, res) => {
+    res.send("Backend is running successfully 🚀");
+});
 
 // Middleware
 app.use(cors());
